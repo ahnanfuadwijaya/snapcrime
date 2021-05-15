2ad34d69689b796108eb151577d5dfea75d0ce67
Ahnan Fuadwijaya <ahnanfuadwijaya@gmail.com>
2021-05-15 18:37:35 +0700

    initial commit

---
+++ src/main/kotlin/id/riverflows/snapcrimerest/SnapCrimeRestApplication.kt
@@ -0,0 +1,11 @@
+package id.riverflows.snapcrimerest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnapCrimeRestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnapCrimeRestApplication>(*args)
+}
---
+++ src/test/kotlin/id/riverflows/snapcrimerest/SnapCrimeRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.riverflows.snapcrimerest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnapCrimeRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
